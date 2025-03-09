86ef5d01b4616878e8ac6206c192b66660db018f
Yogesh Kumar <yogeshkumar@Yogeshs-MacBook-Air.local>
2025-03-09 06:43:17 +0530

    For Deploying

--- Documents/gitUP/app.js
+++ Documents/gitUP/app.js
@@ -1,6 +1,7 @@
-// require("dotenv").config();
+require("dotenv").config();
 const http=require("http");
 const PORT=process.env.PORT || 8000;   // it is for deploying it
+const URL=process.env.MONGO_URL;
 const express=require("express");
 const path=require("path");
 const router=require("./routes/user");
@@ -8,12 +9,12 @@ const blogRouter=require("./routes/blog");
 const mongoose=require("mongoose");
 const cookieParser=require("cookie-parser");
 const {checkForAuthentication}=require("./middlewares/auth");
-const Blog=require("./models/blog");lcv
+const Blog=require("./models/blog");
 const app=express();
 // -- creating server
 http.createServer(app);
 // connection to MongoDB
-mongoose.connect("mongodb+srv://123102075:REgRqMYllpUpL6nq@demo01.nsxw2fh.mongodb.net/?retryWrites=true&w=majority&appName=Demo01")
+mongoose.connect(URL)
 .then(()=>{console.log("MongoDB Connected");});
 // -- Middlewares
 app.use(express.static(path.resolve("./public")));
@@ -34,6 +35,5 @@ app.get("/",async(req,res)=>{
     });
 });
 app.listen(PORT,()=>{console.log("Server Started at : ",PORT);});
-
 // means while only developing not at deploying time  (npm i nodemon -D)
 
